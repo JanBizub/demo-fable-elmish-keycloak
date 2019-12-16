455eca2a26417636357ccec2508b2a5b511b4edd
Jan Bizub <jan.bizub@outlook.com>
2019-12-16 14:44:36 +0100

    prepare project

---
+++ src/Fable.Keycloak/Fable.Keycloak.fs
@@ -0,0 +1,5 @@
+﻿namespace Fable.Keycloak
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/KeycloakDemo.App/src/App.fs
@@ -0,0 +1,56 @@
+﻿module App
+open Elmish
+open Elmish.Navigation
+open Elmish.React
+open Elmish.Debug
+open Fable.React
+open Fable.React.Props
+open Thoth.Json
+open Fetch
+
+// => Authentication - KEYCLOAK ============================================================================================
+let aad_clientID  = "<CLIENT ID>"
+let aad_authority = "https://login.microsoftonline.com/<TENANT ID>"
+let aad_replyUrl  = "https://localhost:1010/"
+
+// => App Types ========================================================================================================
+type AppModel = 
+  { isUiLoading : bool
+    nameFromAPI : string }
+  static member Empty =
+    { isUiLoading = false
+      nameFromAPI = "" }
+
+type Msg =
+  | Login
+  | Logout
+
+// => App State ========================================================================================================
+let update msg model =
+  match msg with
+  | Login ->  
+    model, []
+
+  | Logout ->
+    model, []
+  
+let init _ = 
+ AppModel.Empty, []
+
+// => View to render ===================================================================================================
+let appView model dispatch =
+  div [] [
+    hr []
+    h1 [] [str "Keycloak Demo App"]
+    hr []
+    hr [] ]
+
+Program.mkProgram init update appView
+#if DEBUG
+|> Program.withConsoleTrace
+#endif
+|> Program.withReactBatched "keycloak-demo-app"
+//#if DEBUG
+|> Program.withDebugger
+//#endif
+|> Program.run
